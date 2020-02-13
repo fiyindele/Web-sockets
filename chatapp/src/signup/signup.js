27d3e5b34a8cb56fbb9119d81d480980af58403c
fiyin Oladele <fiyin_oladele@yahoo.com>
2020-02-13 03:44:10 -0800

    routing

---
+++ chatapp/src/signup/signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class SignupComponent extends React.Component {
+
+    render() {
+        return(<div>Hello world from Sign up</div>);
+    }
+}
+
+export default SignupComponent;
